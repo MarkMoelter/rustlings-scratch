5259f8b3a55357b76f71a8ca5e91a62a3629a42f
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2026-06-12 23:30:38 -0400

    feat: day 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 fn main() {
-    let name: &str = "Mark";
-    let age: u32 = 123;
-    let height_meters: f32 = 3.5;
-    let is_devops: bool = true;
+    // Day 2 - Variables, Mutability, and Types
+    // let name: &str = "Mark";
+    // let age: u32 = 123;
+    // let height_meters: f32 = 3.5;
+    // let is_devops: bool = true;
 
-    println!("{name}, {age}, {height_meters}, {is_devops}");
+    // println!("{name}, {age}, {height_meters}, {is_devops}");
+
+    // Day 3 - FizzBuzz
+    let iter = 20;
+
+    for i in 1..=iter {
+        let mut output = i.to_string();
+
+        if i % 3 == 0 {
+            output = String::from("Fizz");
+        }
+        if i % 5 == 0 {
+            output = String::from("Buzz");
+        }
+        if i % 3 == 0 && i % 5 == 0 {
+            output = String::from("FizzBuzz");
+        }
+
+        println!("{output}")
+    }
 }
