ae88d8faca4c9cc067d71f2c806418f57941f6b9
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2026-06-12 23:30:51 -0400

    refactor: improve using match

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,28 @@ fn main() {
     // Day 3 - FizzBuzz
     let iter = 20;
 
+    // for i in 1..=iter {
+    //     let mut output = i.to_string();
+
+    //     if i % 3 == 0 {
+    //         output = String::from("Fizz");
+    //     }
+    //     if i % 5 == 0 {
+    //         output = String::from("Buzz");
+    //     }
+    //     if i % 3 == 0 && i % 5 == 0 {
+    //         output = String::from("FizzBuzz");
+    //     }
+
+    //     println!("{output}")
+    // }
+
     for i in 1..=iter {
-        let mut output = i.to_string();
-
-        if i % 3 == 0 {
-            output = String::from("Fizz");
+        match (i % 3, i % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{i}"),
         }
-        if i % 5 == 0 {
-            output = String::from("Buzz");
-        }
-        if i % 3 == 0 && i % 5 == 0 {
-            output = String::from("FizzBuzz");
-        }
-
-        println!("{output}")
     }
 }
